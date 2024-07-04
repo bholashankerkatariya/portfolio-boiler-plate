28b455c0de2bad2406ed852df6a0a39618694d55
Bhola Shanker <50939200+bholashankerkatariya@users.noreply.github.com>
2024-07-04 09:42:02 +0530

    main.jsx location change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx';
-import { ThemeProvider } from './src/common/ThemeContext.jsx';
+import App from './App.jsx';
+import { ThemeProvider } from './common/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
